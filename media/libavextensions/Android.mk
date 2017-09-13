ebec8e7a0a46dc99935fadf82059bdfbf78c50ce
jumoog <kilianvonpflugk@gmail.com>
2017-09-13 15:37:58 +0200

    fix path

--- media/libavextensions/Android.mk
+++ media/libavextensions/Android.mk
@@ -15,7 +15,7 @@ LOCAL_C_INCLUDES:= \
         $(TOP)/frameworks/native/include/media/openmax \
         $(TOP)/external/flac/include \
         $(TOP)/system/core/base/include \
-        $(TOP)/hardware/qcom/media/mm-core/inc
+        $(TOP)/hardware/qcom/media-caf/msm8998/mm-core/inc
 
 LOCAL_SHARED_LIBRARIES += \
         libhidlbase \
@@ -58,7 +58,7 @@ LOCAL_C_INCLUDES:= \
         $(TOP)/frameworks/native/include/media/openmax \
         $(TOP)/external/flac/include \
         $(TOP)/system/core/base/include \
-        $(TOP)/hardware/qcom/media/mm-core/inc
+        $(TOP)/hardware/qcom/media-caf/msm8998/mm-core/inc
 
 LOCAL_SHARED_LIBRARIES += \
         libhidlbase \
